0c27dc23a578f0f042be349aa94b5914071bd0a2
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-03-25 20:43:55 +0800

    added build packs

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const app = express();
-
-// Serve all the files in '/dist' directory
-app.use(express.static('dist'));
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
